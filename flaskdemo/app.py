ed9820960307d37fb4fca1608c0f0c4270d55647
Michael Jacobs <mike32jacobs@gmail.com>
2022-03-24 17:17:30 +0000

    try

--- flaskdemo/app.py
+++ flaskdemo/app.py
@@ -1,6 +1,6 @@
-# hello.py
+# app.py
 from flask import Flask    
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask! This is the Hello.py file"
+    return "Hello, Flask!"
